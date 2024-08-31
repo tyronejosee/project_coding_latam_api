857cff0f4e12d14c8f67cebc76941a39648e3c6c
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-08-30 21:53:28 -0400

    fix: add hosts

--- app/core/config.py
+++ app/core/config.py
@@ -21,6 +21,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://proyecto-web-react.vercel.app",
     ]
 
     class Config:
